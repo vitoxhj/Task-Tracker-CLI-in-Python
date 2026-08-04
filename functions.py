61e69a303f6d85f485cb2091f3a235af2a69eaea
Vitox <vitoxhen@gmail.com>
2026-08-04 00:32:39 -0300

    add update

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import json
 import os
+from time import sleep
 
 def create_json(arquive,dados):
     if not os.path.exists(arquive):
@@ -7,3 +8,34 @@ def create_json(arquive,dados):
             json.dump(dados, f, indent=4, ensure_ascii=False)
     else:
         return
+
+def create(tasks):
+    name = str(input('Task:'))
+    info = {
+        'id': len(tasks)+1,
+        'name': name,
+        'completed': False,
+        'In_progress':False
+    }
+    tasks.append(info)
+
+    with open('tasks.json', 'w', encoding='utf-8') as f:
+        json.dump(tasks, f, indent=4, ensure_ascii=False)
+    print('Task added successfully!')
+    sleep(1)
+
+def update(tasks):
+    id = int(input('ID:'))
+    for task in tasks:
+        if task['id'] == id:
+            print(f'old: {task['name']}')
+            new = str(input('New:'))
+            task['name'] = new
+            with open('tasks.json', 'w', encoding='utf-8') as f:
+                json.dump(tasks, f, indent=4, ensure_ascii=False)
+            print('Task updated successfully!')
+            sleep(1)
+            return
+    print('ID not found!')
+    sleep(1)
+    return
--- main.py
+++ main.py
@@ -3,27 +3,36 @@ from time import sleep
 import json
 import functions
 
+#create json
 functions.create_json('tasks.json', [])
 
+#open json
 with open('tasks.json', 'r', encoding='utf-8') as f:
     tasks = json.load(f)
 
 
 while True:
+    #Interface
     try:
         os.system('cls')
         sleep(0.5)
         print('='*30)
         print('TASK MANAGER'.center(30))
         print('='*30)
-        print('1-add\n2-update\n3-delete\n4-mark\n5-list\n6-completed list\n7-not completed list\n8-list in progress')
+        print('1-add\n2-update\n3-delete\n4-mark\n5-list\n6-completed list\n7-not completed list\n8-list in progress\n9-exit')
         print('='*30)
         option = int(input('->'))
+        #Erro
         if option <= 0 or option >= 10:
             print('option unavailable')
             sleep(1)
+        #Create a task
         elif option == 1:
-            pass
+            functions.create(tasks)
+
+        #update a task
+        elif option == 2:
+            functions.update(tasks)
     except ValueError:
         sleep(1)
         print('Value error')
